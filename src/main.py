3ecf2406ce552bfa40f2157163def807f925bb61
evgenanato <evgenanato@yandex.ru>
2026-06-10 09:35:09 +0500

    first endpoint

--- src/main.py
+++ src/main.py
@@ -2,7 +2,13 @@ import uvicorn
 
 from fastapi import FastAPI
 
-app = FastAPI
+app = FastAPI()
+
+
+@app.get("/")
+async def welcome() -> dict:
+    return {"message": "Hello!!!"}
+
 
 if __name__ == "__main__":
     uvicorn.run("main:app", reload=True)
