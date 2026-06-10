0a9517084d8d03ca8ac6c77854d86d9f89350037
evgenanato <evgenanato@yandex.ru>
2026-06-10 10:14:54 +0500

    add view profile endpoint

--- src/main.py
+++ src/main.py
@@ -10,6 +10,11 @@ async def welcome() -> dict:
     return {"message": "Hello!!!"}
 
 
+@app.get("/user/profile")
+async def profile() -> dict:
+    return {"profile": "View profile user"}
+
+
 @app.get("/hello/{user}")
 async def welcome_user(user: str) -> dict:
     return {"user": f"Hello, {user}"}
