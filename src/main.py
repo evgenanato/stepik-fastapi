8afc7d5af6872b3ea653244fe7064d7189b6b866
evgenanato <evgenanato@yandex.ru>
2026-06-10 09:59:05 +0500

    add welcom_user endpoint

--- src/main.py
+++ src/main.py
@@ -10,5 +10,10 @@ async def welcome() -> dict:
     return {"message": "Hello!!!"}
 
 
+@app.get("/hello/{user}")
+async def welcome_user(user: str) -> dict:
+    return {"user": f"Hello, {user}"}
+
+
 if __name__ == "__main__":
     uvicorn.run("main:app", reload=True)
