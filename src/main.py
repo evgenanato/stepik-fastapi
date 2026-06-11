ae76881cdbe94de6687ccc90738f4eb96acc4e15
evgenanato <evgenanato@yandex.ru>
2026-06-11 13:24:26 +0500

    stepik 2.4 module Path and Query

--- src/main.py
+++ src/main.py
@@ -1,27 +1,53 @@
+from typing import Annotated
+
 import uvicorn
 
-from fastapi import FastAPI
+from fastapi import FastAPI, Path, Query
 
 app = FastAPI()
 
-
-@app.get("/")
-async def welcome() -> dict:
-    return {"message": "Hello!!!"}
-
-
-@app.get("/user/profile")
-async def profile() -> dict:
-    return {"profile": "View profile user"}
+country_dict = {
+    "Russia": ["Moscow", "St. Petersburg", "Novosibirsk", "Ekaterinburg", "Kazan"],
+    "USA": ["New York", "Los Angeles", "Chicago", "Houston", "Philadelphia"],
+}
 
 
-@app.get("/products/{product_id}")
-async def detail_view(product_id: int) -> dict:
-    return {"product": f"Stock number {product_id}"}
+@app.get("/user/{username}")
+async def login(
+    username: Annotated[
+        str,
+        Path(
+            min_length=3,
+            max_length=15,
+            description="Enter your username",
+            examples=[
+                "permin0ff",
+            ],
+        ),
+    ],
+    first_name: str | None = Query(
+        default=None,
+        max_length=10,
+    ),
+) -> dict:
+    return {"user": username, "Name": first_name}
 
-@app.get("/hello/{user}")
-async def welcome_user(user: str) -> dict:
-    return {"user": f"Hello, {user}"}
+
+@app.get("/user")
+async def search(people: Annotated[list[str], Query()]) -> dict:
+    return {"user": people}
+
+
+@app.get("/country/{country}")
+async def list_cities(country: str, limit: int) -> dict:
+    if country in country_dict:
+        cities = country_dict[country]
+    return {"country": country, "cities": cities[0:limit]}
+
+
+@app.get("/users/{name}/{age}")
+async def users(name: str, age: int) -> dict:
+    return {"user_name": name, "user_age": age}
 
 
 if __name__ == "__main__":
