6f36ad43e445bc7b36be834248a7ef0526f180d1
evgenanato <evgenanato@yandex.ru>
2026-06-15 13:48:27 +0500

    stepik CRUD module v2

--- src/crud.py
+++ src/crud.py
@@ -3,54 +3,55 @@ from fastapi import FastAPI, HTTPException, status, Body
 
 app = FastAPI()
 
-messages_db = {0: "First post in FastAPI"}
+
+comments_db = {0: "First comment in FastAPI"}
 
 
-@app.get("/messages")
-async def read_messages() -> dict:
-    return messages_db
+@app.get("/comments")
+async def read_comments() -> dict:
+    return comments_db
 
 
-@app.get("/messages/{message_id}")
-async def read_message(message_id: int) -> str:
+@app.get("/comments/{comment_id}")
+async def read_comment(comment_id: int) -> str:
     try:
-        return messages_db[message_id]
+        return comments_db[comment_id]
     except:
-        raise HTTPException(status_code=404, detail="Message not found")
+        raise HTTPException(status_code=404, detail="Comment not found")
 
 
-@app.post("/messages", status_code=status.HTTP_201_CREATED)
-async def create_message(message: str = Body()) -> str:
-    current_index = max(messages_db) + 1 if messages_db else 0
-    messages_db[current_index] = message
-    return "Message created!"
+@app.post("/comments", status_code=status.HTTP_201_CREATED)
+async def create_comment(comment: str = Body()) -> str:
+    current_index = max(comments_db) + 1 if comments_db else 0
+    comments_db[current_index] = comment
+    return "Comment created!"
 
 
-@app.put("/messages/{message_id}", status_code=status.HTTP_200_OK)
-async def update_message(message_id: int, message: str = Body()) -> str:
-    if message_id in messages_db:
-        messages_db[message_id] = message
-        return "Message updated"
+@app.put("/comments/{comment_id}", status_code=status.HTTP_200_OK)
+async def update_comment(comment_id: int, comment: str = Body()) -> str:
+    if comment_id in comments_db:
+        comments_db[comment_id] = comment
+        return "Comment updated!"
     else:
         raise HTTPException(
-            status_code=status.HTTP_404_NOT_FOUND, detail="Message not found"
+            status_code=status.HTTP_404_NOT_FOUND, detail="Comment not found"
         )
 
 
-@app.delete("/messages/{message_id}", status_code=status.HTTP_200_OK)
-async def delete_message(message_id: int) -> str:
-    if message_id not in messages_db:
+@app.delete("/comments/{comment_id}", status_code=status.HTTP_200_OK)
+async def delete_comment(comment_id: int) -> str:
+    if comment_id not in comments_db:
         raise HTTPException(
-            status_code=status.HTTP_404_NOT_FOUND, detail="Message not found"
+            status_code=status.HTTP_404_NOT_FOUND, detail="Comment not found"
         )
-    messages_db.pop(message_id)
-    return f"Message ID={message_id} deleted!"
+    comments_db.pop(comment_id)
+    return "Comment deleted!"
 
 
-@app.delete("/messages", status_code=status.HTTP_200_OK)
-async def delete_messages() -> str:
-    messages_db.clear()
-    return "Clear message_db"
+@app.delete("/comments", status_code=status.HTTP_200_OK)
+async def delete_comments() -> str:
+    comments_db.clear()
+    return "Clear comment_db"
 
 
 if __name__ == "__main__":
