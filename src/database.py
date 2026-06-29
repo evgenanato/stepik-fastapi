cd019d200e44a5a9cb2efa3b5a456cba1bec00ee
evgenanato <evgenanato@yandex.ru>
2026-06-29 21:45:04 +0500

    stepik create database.py

---
+++ src/database.py
@@ -0,0 +1,13 @@
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker, DeclarativeBase
+
+# Строка подключения для SQLite
+DATABASE_URL = "sqlite:///ecommerce.db"
+
+# Создаём Engine
+engine = create_engine(DATABASE_URL, echo=True)
+
+SessionLocal = sessionmaker(bind=engine)
+
+class Base(DeclarativeBase):  # New
+    pass
