9e4119a68224ef0f48e57e1265084e705b3de021
evgenanato <evgenanato@yandex.ru>
2026-07-01 17:23:05 +0500

    stepik category and product models 6.5

---
+++ src/models/categories.py
@@ -0,0 +1,17 @@
+from sqlalchemy import Integer, String, Boolean
+from sqlalchemy.orm import Mapped, mapped_column, relationship  # New
+
+from models.products import Product
+from src.database import Base
+
+
+class Category(Base):
+    __tablename__ = "categories"
+
+    id: Mapped[int] = mapped_column(Integer, primary_key=True)
+    name: Mapped[str] = mapped_column(String(50), nullable=False)
+    is_active: Mapped[bool] = mapped_column(Boolean, default=True)
+
+    products: Mapped[list["Product"]] = relationship(
+        "Product", back_populates="category"
+    )  # New
